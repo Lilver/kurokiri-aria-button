ca771a4ae2e4cbc6956bb3fe91d7ad992ace8ae2
陈铒东 <chenerdong@hupu.com>
2020-10-28 17:59:47 +0800

    code save

--- src/components/Menu/index.jsx
+++ src/components/Menu/index.jsx
@@ -2,10 +2,9 @@ import React from 'react'
 import Style from './styles.module.less';
 
 export default function Menu() {
-
   return (
     <div className={Style.container}>
-      
+      黑桐亚里亚（黑桐アリア）
     </div>
   )
 }
